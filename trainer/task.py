8dfe410b1281228182a63d471f8c0362975fe751
andydevs <akanshul97@gmail.com>
2021-03-13 22:16:11 -0500

    Just training model on entire dataset for now. Will implement train/test split some other way

--- trainer/task.py
+++ trainer/task.py
@@ -2,7 +2,6 @@
 Load data and train model
 """
 import tensorflow as tf
-from tensorflow.keras.layers.experimental.preprocessing import StringLookup, CategoryEncoding
 from .model import MushroomClassifierModel
 from argparse import ArgumentParser
 
@@ -13,7 +12,6 @@ MODEL_FILE = 'files/models/saved-model.h5'
 # Defaults
 DEF_BATCH_SIZE = 200
 DEF_EPOCHS = 10
-TRAIN_FRAC = 0.7
 
 def get_data(batch_size=DEF_BATCH_SIZE):
     """
@@ -21,11 +19,11 @@ def get_data(batch_size=DEF_BATCH_SIZE):
     """
     # Get dataset from csv
     dataset = tf.data.experimental.make_csv_dataset(
-        file_pattern=,
+        file_pattern=DATA_FILE,
         batch_size=batch_size,
         label_name='class')
 
-    # Preprocess label
+    # Preprocess labels
     def preprocess_label(features, labels):
         """
         Convert string-categorical labels into
@@ -38,34 +36,28 @@ def get_data(batch_size=DEF_BATCH_SIZE):
         return features, labels
     dataset = dataset.map(preprocess_label)
 
-    # Split into training and testing
-    train_num = int(TRAIN_FRAC*len(dataset))
-    train_dataset = dataset.take(train_num)
-    test_dataset = dataset.skip(train_num)
-
     # Return dataset
-    return train_dataset, test_dataset
+    return dataset
 
 
-def train_and_evaluate_model(batch_size=DEF_BATCH_SIZE, epochs=DEF_EPOCHS):
+def train_and_save_model(batch_size=DEF_BATCH_SIZE, epochs=DEF_EPOCHS):
     """
-    Train and evaluate model. Save model afterwards.
+    Train model. Save model afterwards.
     """
-    train_dataset, test_dataset = get_data(batch_size)
+    dataset = get_data(batch_size)
     model = MushroomClassifierModel()
     model.compile(
         optimizer='adam',
         loss='binary_crossentropy',
         metrics=['accuracy'])
-    model.fit(train_dataset, epochs=epochs)
-    model.evaluate(test_dataset)
+    model.fit(dataset, epochs=epochs)
     model.save(MODEL_FILE)
 
 
 if __name__ == '__main__':
     # Define argument parser
     parser = ArgumentParser(
-        description='Train and evaluate classifier model')
+        description='Train and save classifier model')
     parser.add_argument(
         '--batch',
         type=int,
@@ -82,6 +74,6 @@ if __name__ == '__main__':
 
     # Parse args and train model
     args = parser.parse_args()
-    train_and_evaluate_model(
+    train_and_save_model(
         batch_size=args.batch_size,
         epochs=args.epochs)
