3e25a0ea33b0e0b442b628ebec3ee22a5b951ff8
Andydevs <akanshul97@gmail.com>
2021-03-17 19:22:57 -0400

    Preprocess labels before creating dataset

--- trainer/data.py
+++ trainer/data.py
@@ -4,28 +4,21 @@ Data input pipeline
 import tensorflow as tf
 import pandas as pd
 
+# Possible label categories
+label_categories = ['e', 'p']
 
 def get_data(data_file, batch_size, shuffle_buffer, repeat_num, train_frac, display_data):
     """
     Get data from data file
     """
-    # Read dataset. Split features and labels
+    # Read dataset. Split features and labels. Convert labels to indeces
     df = pd.read_csv(data_file)
     labels = df.pop('class')
+    labels = labels.apply(label_categories.index)
     dataset = tf.data.Dataset.from_tensor_slices((dict(df), labels))
 
     # Preprocess labels
-    def preprocess_label(features, labels):
-        """
-        Convert string-categorical labels into
-        binary one-hot representation
-        """
-        true_label = tf.constant('p')
-        labels = tf.math.equal(labels, true_label)
-        labels = tf.cast(labels, tf.uint8)
-        labels = tf.one_hot(labels, depth=2)
-        return features, labels
-    dataset = dataset.map(preprocess_label)
+    dataset = dataset.map(lambda feats, labels: (feats, tf.one_hot(labels, depth=2)))
 
     # Shuffle, batch, and repeat dataset
     dataset = dataset.shuffle(shuffle_buffer)
