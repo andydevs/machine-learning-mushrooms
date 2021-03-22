61a33b27471ba6f87493d58a1e70d5cfdd50aebb
Andydevs <akanshul97@gmail.com>
2021-03-21 20:16:45 -0400

    Created prediction step

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,29 @@
 """
 Main flask app
 """
+import tensorflow as tf
+import numpy as np
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from .form import MushroomForm
+
+# Load model
+model = tf.keras.models.load_model('files/models/saved-model.tf')
+
+def predict(form_data):
+    # Remove submit and csrf token
+    del form_data['submit']
+    del form_data['csrf_token']
+    # Refactor names and replace characters with numpy arrays
+    form_data = { 
+        k.replace('_', '-'):np.char.array(v)
+        for k,v in form_data.items() }
+
+    # Run prediction
+    result = model.predict(form_data)
+
+    # Return first instance values
+    return { 'e': result[0,0], 'p': result[0,1] }
 
 # Create and configure app
 app = Flask(__name__)
@@ -13,7 +33,7 @@ Bootstrap(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = MushroomForm()
-    result = { 'e': 0.11, 'p': 0.89 }
+    result = None
     if request.method == 'POST':
-        pass
+        result = predict(form.data)
     return render_template('index.html', form=form, result=result)
